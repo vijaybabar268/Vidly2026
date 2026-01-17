20080ff119958ea98f1f698131667e01eec9cbeb
vijaybabar <babarvm@gmail.com>
2026-01-18 00:33:37 +0530

    Initial commit - Created MVC web application

---
+++ Vidly2026/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vidly2026.MvcApplication" Language="C#" %>
---
+++ Vidly2026/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly2026.Startup))]
+namespace Vidly2026
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
