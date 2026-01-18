0bd8c8863d8d8374ac53fde0c144cafb31cc6834
vijaybabar <babarvm@gmail.com>
2026-01-18 22:39:49 +0530

    Performs CRUD operations around customers

--- Vidly2026/Controllers/CustomersController.cs
+++ Vidly2026/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly2026.Models;
+using Vidly2026.ViewModels;
 
 namespace Vidly2026.Controllers
 {
@@ -29,6 +30,58 @@ namespace Vidly2026.Controllers
 
             return View(customers);
         }
+                
+        // GET: Customers/New
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        // POST: Customers/Save
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index","Customers");
+        }
+
+        // GET: Customers/Edit/1
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.FirstOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = _context.MembershipTypes.ToList(),
+                Customer = customer
+            };
+
+            return View("CustomerForm", viewModel);
+        }
 
         // GET: Customers/Details/1
         public ActionResult Details(int id)
@@ -39,6 +92,21 @@ namespace Vidly2026.Controllers
                 return HttpNotFound();
 
             return View(customer);
+        }
+
+        // DELETE: Customers/Delete/1
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var customer = _context.Customers.Include(m => m.MembershipType).FirstOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
         }
     }
 }
---
+++ Vidly2026/ViewModels/NewCustomerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly2026.Models;
+
+namespace Vidly2026.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
