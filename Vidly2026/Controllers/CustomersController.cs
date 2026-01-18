8da614b81cce64b3616af2d27b31f2b635623665
vijaybabar <babarvm@gmail.com>
2026-01-18 15:51:50 +0530

    End of the section 2 - Fundamentals of MVC

---
+++ Vidly2026/Controllers/CustomersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2026.Models;
+
+namespace Vidly2026.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        // GET: Customers/Details/1
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().FirstOrDefault(c => c.Id == id);
+
+            return View(customer);
+        }
+
+        private List<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id=1, Name = "John Smith"},
+                new Customer { Id=2, Name = "Mary Williams"}
+            };
+        }
+
+    }
+}
--- Vidly2026/Controllers/MoviesController.cs
+++ Vidly2026/Controllers/MoviesController.cs
@@ -4,16 +4,53 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly2026.Models;
+using Vidly2026.ViewModels;
 
 namespace Vidly2026.Controllers
 {
     public class MoviesController : Controller
     {
+        // GET: Movies/Index
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        // GET: Movies/Details/1
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().FirstOrDefault(m => m.Id == id);
+
+            return View(movie);
+        }
+
+        private List<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id=1, Name = "Shrek" },
+                new Movie { Id=2, Name = "Wall-e" }
+            };
+        }
+
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name="Customer 1"},
+                new Customer {Name="Customer 2"}
+            };
 
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
     }
 }
