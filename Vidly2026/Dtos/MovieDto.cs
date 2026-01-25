1c52f210729cdda78ea67b008763b7dccd190915
Vijaybabar <babarvm@gmail.com>
2026-01-25 16:53:30 +0530

    Building restful web api around movies

---
+++ Vidly2026/Dtos/MovieDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly2026.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        
+        public DateTime ReleaseDate { get; set; }
+                
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+                
+        [Required]        
+        public byte GenreId { get; set; }
+    }
+}
