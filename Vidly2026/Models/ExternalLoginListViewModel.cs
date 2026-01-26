c97d3f7fcdb99b2c155ac2d043041d3b0c23655c
Vijaybabar <babarvm@gmail.com>
2026-01-26 16:27:23 +0530

    Refactor code - Separated classes by its file name

---
+++ Vidly2026/Models/ExternalLoginListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Vidly2026.Models
+{
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ Vidly2026/Models/ForgotPasswordViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly2026.Models
+{
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
