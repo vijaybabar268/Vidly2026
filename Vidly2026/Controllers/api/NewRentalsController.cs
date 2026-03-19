7280f458da50d88a556df5b5d6dba083ce49cc9a
Vijaybabar <babarvm@gmail.com>
2026-03-19 22:17:20 +0530

    Building feature end to end - 1 Start define domain modeling

---
+++ Vidly2026/Controllers/api/NewRentalsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly2026.Dtos;
+
+namespace Vidly2026.Controllers.api
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vidly2026/Dtos/NewRentalDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly2026.Dtos
+{
+    public class NewRentalDto
+    {
+        public int CustomerId { get; set; }
+
+        public List<int> MovieIds { get; set; }
+
+    }
+}
