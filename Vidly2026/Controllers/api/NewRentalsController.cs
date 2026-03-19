7697b9ba1ada2498bb5c5c4b0f63670c5cf1b814
Vijaybabar <babarvm@gmail.com>
2026-03-19 22:33:46 +0530

    Added CreateNewRental simple endpoint

--- Vidly2026/Controllers/api/NewRentalsController.cs
+++ Vidly2026/Controllers/api/NewRentalsController.cs
@@ -5,15 +5,41 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Vidly2026.Dtos;
+using Vidly2026.Models;
 
 namespace Vidly2026.Controllers.api
 {
     public class NewRentalsController : ApiController
     {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            throw new NotImplementedException();
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.UtcNow
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
