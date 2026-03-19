87111041ddd19fc656ca3bae90c538d82fd97d67
Vijaybabar <babarvm@gmail.com>
2026-03-19 23:11:09 +0530

    Stated to Building the front end

---
+++ Vidly2026/Controllers/RentalsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly2026.Controllers
+{
+    public class RentalsController : Controller
+    {
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
